479c6ca9397aa040355e2fd39ba9a00c699fbb46
Sam <s.dumble@stats4sd.org>
2022-01-24 17:07:24 +0000

    adding knitr

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ library(vegan) # Shannon Diversity Index
 library(dplyr)
 library(DT)
 library(leaflet)
-#library(kableExtra)
+library(knitr)
 
 source("download_nasapower.R")
 # Define UI for application that draws a histogram
